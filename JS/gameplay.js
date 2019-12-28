9b853bbf77a65ee238b4f7b313a14c4146e28f3c
edwin gonzalez <edwin.gonzalez11@hotmail.com>
2019-12-27 22:40:23 -0500

    final JS

--- JS/gameplay.js
+++ JS/gameplay.js
@@ -1,8 +1,14 @@
 var ComputerChoices = ["r", "p", "s"];
 
+var wins = 0;
+var losses = 0;
+var ties = 0;
+
 var UserChoiceText = document.getElementById("userchoice-text");
 var ComputerChoiceText = document.getElementById("computerchoice-text");
-
+var winsText = document.getElementById("wins-text")
+var lossesText = document.getElementById("losses-text");
+var tiestext = document.getElementById("ties-text");
 document.onkeyup = function (event) {
 
     var userGuess = event.key;
@@ -10,8 +16,23 @@ document.onkeyup = function (event) {
     var ComputerGuess = ComputerChoices[Math.floor(Math.random() * ComputerChoices.length)];
 
     if ((userGuess === "r") || (userGuess === "p") || (userGuess === "s")) {
+        // Generating wins/ losses / ties using (IF) statements
+        if ((userGuess === "r" && ComputerGuess === "s") ||
+            (userGuess === "s" && ComputerGuess === "p") ||
+            (userGuess === "p" && ComputerGuess === "r")) {
+            wins++;
+        } else if (userGuess === ComputerGuess) {
+            ties++;
+        } else {
+            losses++;
+        }
+
+        // 
         UserChoiceText.textContent = "You chose: " + userGuess;
         ComputerChoiceText.textContent = " Computer Chose: " + ComputerGuess;
+        winsText.textContent = "Wins: " + wins;
+        lossesText.textContent = "Losses: " + losses;
+        tiestext.textContent = " ties: " + ties;
     }
 
 };
